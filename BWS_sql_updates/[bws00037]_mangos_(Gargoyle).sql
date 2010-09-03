ea5b758673111cf9ee6c95c31c30b9575cfc9559
mosst <Makcim_Chaos@ukr.net>
2010-09-03 15:36:44 +0300

    add sql for bws37.

--- BWS_sql_updates/[bws00037]_mangos_(Gargoyle).sql
+++ BWS_sql_updates/[bws00037]_mangos_(Gargoyle).sql
@@ -1,2 +1,6 @@
 -- DK Gargoyle
 UPDATE `creature_template` SET `ScriptName` = 'npc_death_knight_gargoyle' WHERE `entry` = '27829';
+-- Gargoyle Strike AP bonus
+DELETE FROM spell_bonus_data WHERE entry IN (51963);
+INSERT INTO spell_bonus_data VALUES
+(51963, 0, 0, 0.42, 'Gargoyle Strike');
