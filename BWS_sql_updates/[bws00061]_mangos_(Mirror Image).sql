cc277591b692b41cedec9b56509954bffbc32d0d
mosst <makcim_Chaos@ukr.net>
2010-09-26 18:04:54 +0300

    [bws62] support Stranglethorn Vale fishing game event.

--- BWS_sql_updates/[bws00061]_mangos_(Mirror Image).sql
+++ BWS_sql_updates/[bws00061]_mangos_(Mirror Image).sql
@@ -1,5 +1,5 @@
 -- Mirror Immage
-UPDATE creature_template SET speed_walk = 2.5, modelid_A = 11686,minlevel = 80, maxlevel = 80, AIName = '', ScriptName='npc_mirror_image' WHERE entry = 31216;
+UPDATE creature_template SET speed_walk = 2.5, modelid_1 = 11686, modelid_3 = 11686, minlevel = 80, maxlevel = 80, AIName = '', ScriptName='npc_mirror_image' WHERE entry = 31216;
 DELETE FROM creature_ai_scripts WHERE creature_id = 31216;
 
 DELETE FROM spell_bonus_data WHERE entry IN (59637,59638);
