fd3b6a181185683daabdebcea138eec8cfcc3133
PSZ <zavatski@gmail.com>
2010-09-29 16:24:01 +0300

    Remove unneeded queries

--- BWS_sql_updates/[bws00071]_mangos_(Q Powering Our Defenses).sql
+++ BWS_sql_updates/[bws00071]_mangos_(Q Powering Our Defenses).sql
@@ -4,8 +4,4 @@ UPDATE creature_template SET ScriptName='npc_infused_crystal' WHERE entry = 1636
 
 -- Enraged Wraith
 DELETE FROM creature WHERE id = 17086;
-UPDATE creature_template SET AIName='EventAI', flags_extra=flags_extra|64 WHERE entry=17086;
-
-DELETE FROM creature_ai_scripts WHERE creature_id = 17086;
-INSERT INTO creature_ai_scripts VALUES
-(1708601,17086,2,0,100,0,25,0,0,0,11,8599,0,1,1,-106,0,0,0,0,0,0,'Enraged Wraith - Cast Enrage on 50% HP');
+UPDATE creature_template SET flags_extra=flags_extra|64 WHERE entry=17086;
