a9853d832344d395e21769901721a5c0c6198b5a
mosst <makcim_Chaos@ukr.net>
2010-09-30 15:43:28 +0300

    fix typo sql script name

--- BWS_sql_updates/[bws00018]_mangos_(Quest Skywing).sql
+++ BWS_sql_updates/[bws00018]_mangos_(Quest Skywing).sql
@@ -1,4 +1,4 @@
 -- Quest Skywing
-UPDATE creature_template SET speed_walk = 1.5, ScriptName = 'npc_geezle' WHERE entry = 22424;
+UPDATE creature_template SET speed_walk = 1.5, ScriptName = 'npc_skywing' WHERE entry = 22424;
 UPDATE creature_template SET ScriptName = 'mob_luanga_the_imprisoner' WHERE entry = 18533;
 DELETE FROM creature WHERE id = 18533;
