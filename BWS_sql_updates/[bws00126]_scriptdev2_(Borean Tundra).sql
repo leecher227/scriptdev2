cff9f9e93a7e84adca4a15785558779379f4008e
MaxXx2021 <maxxx2021@mail.ru>
2010-12-27 20:55:28 +0300

    Forgot SQL Updates for Borean Tundra

---
+++ BWS_sql_updates/[bws00126]_scriptdev2_(Borean Tundra).sql
@@ -0,0 +1,23 @@
+DELETE FROM `script_texts` WHERE `comment` = 28498;
+insert into `script_texts` (`entry`,`content_default`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+(-1534071, 'My liege, the infiltratio and control of the Alliance power sctructure by our cultists is well underway.', 'Мой Господин, наше проникновение в энергетическое здание Альянса и установление контроля над ним идут полным ходом!', 14211,0,0,0, '28498'),
+(-1534072, 'The power you\'ve bestowed upon me has granted me great mental influence over human minds. I bear these offerings as proof of my progress.', 'Власть которой ты облек меня даровала мне безграничное влияние на человеческие умы... Прими эти дары как свидетельство моих успехов.', 14212,0,0,0, '28498'),
+(-1534073, 'Your achievements in this region rather impress Prince of Blood. I am pleased!', 'Твои достижения в этом регионе весьма впечетляют Принц Крови. Я доволен! .', 14756,0,0,1, '28498'),
+(-1534074, 'Leryssa!', 'Лерисса!', 14664,1,0,0, '28498'),
+(-1534084, 'Thassarian! You\'re alive!', 'Тассариан! Ты жив!', 14489,0,0,0, '28498'),
+(-1534075, 'What have you done to my sister, you motherless elf scum!?', 'Что ты сделал с моей сестрой, ты, грязная эльфийская тварь!?', 14665,1,0,0, '28498'),
+(-1534076, 'Now this is surprise, Thassarian. I hadn\'t heard from Mograine of the other deathknights for months. You\'ve come to rejoin the Scourge. I take it?', 'Это удивительно, Тассариан. Я не получал известий от Могрейна и других рыцарей смерти уже несколько месяцев. Как я понимаю ты пришол воссоедениться с плетью?', 14757,0,0,1, '28498'),
+(-1534077, 'I would sooner slit my own throat. You will pay for what you did to your own men. Arthas... for what you did do me! I swear it.', 'Я скорее перережу горло самому себе. Ты заплатишь за то, что ты сделал со своими людьми Артас за то, что ты сделал со мной! Клянусь.', 14666,1,0,0, '28498'),
+(-1534078, 'Allow me to take care of the intruders, lord. I will feed their entrails to the maggots.', 'Позволь мне заняться этими чужаками, Господин. Я скормлю их потраха червям.', 14213,0,0,1, '28498'),
+(-1534079, 'Do not fail me, San\'layn. Return to Icecrown with this fool\'s head or do not bother to return.', 'Не подведи меня Санлейн, либо вернись в Ледяную Корону с головой этого глупца, либо не возращайся вовсе!', 14758,0,0,1, '28498'),
+(-1534080, 'Yes, my lord!', 'Да, Мой Господин!', 14214,0,0,0, '28498'),
+(-1534083, 'Leryssa... you... you\'re all right!', 'Лерисса... ты... ты в порядке?', 14667,0,0,0, '28498'),
+(-1534085, 'I thought... I thought you were... dead.', 'Я думала... я думала... ты умер.', 14490,0,0,0, '28498'),
+(-1534087, 'Don\'t leave me again! You want to fight for your country, but they don\'t even want you! They sent you here to die!', 'Не оставляй меня опять! Ты хочешь сражаться за свою страну, но страна этого не хочет! Тебя отправили сюда на верную смерть!', 14491,0,0,0, '28498'),
+(-1534086, 'I cannot return with you just yet, Lerissa. I am not quite done with the Scourge.', 'Я не могу вернуться с тобой, Лерисса. Я еще не разделался с плетью.', 14668,0,0,0, '28498'),
+(-1534088, 'You might be right, sister. My obligations to my land and King have been fulfilled. But there is something that I still owe to myself.', 'Возможно ты права, сестра. Я выполнил свой долг перед страной и королем, но есть кое-что, в чем я поклялся самому cебе.', 14669,0,0,0, '28498'),
+(-1534089, 'I know that look in your eye... I\'m not going to be able to talk you out of this. If you die on me again...', 'Я знаю твой взгяд... Я уже не смогу тебя переубедить. Если я снова тебя потеряю...', 14492,0,0,0, '28498'),
+(-1534090, 'Do not worry, Leryssa. I will come back yo you when I an done. Nothing in the world will stop me from coming home to the only family that I have left.', 'Не тревожся, Лерисса. Я вернусь домой как только выполню задуманное. Ничто на свете не остановит меня на пути к единственным близким людям, которые у меня остались.', 14670,0,0,0, '28498'),
+(-1534081, 'What... what happened to me?', 'Что... что со мной произошло?', 14196,0,0,0, '28498'),
+(-1534082, 'I think... i think i\'m going to faint...', 'Аааа... Как же кружется голова...', 14197,0,0,0, '28498');
+
